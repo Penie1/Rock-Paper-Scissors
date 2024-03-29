2460d25978465c5f6cb88c33f04d2df98680407e
Peniel Bekele <142409326+Penie1@users.noreply.github.com>
2024-03-30 00:51:52 +0300

    Implement game play functionality

--- app.js
+++ app.js
@@ -6,4 +6,32 @@ const getComputerChoice = (choices) => {
   const randomNum = Math.floor(Math.random() * arrLength);
   return choices[randomNum];
 };
-getComputerChoice(gameChoices);
+const computerSelection = getComputerChoice(gameChoices);
+const playerSelection = "rock";
+
+const isPlayerWin = (playerSelection, computerSelection) => {
+  return (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  );
+};
+
+const isPlayerLose = (playerSelection, computerSelection) => {
+  return (
+    (playerSelection === "scissors" && computerSelection === "rock") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "rock" && computerSelection === "paper")
+  );
+};
+
+const playRound = (playerSelection, computerSelection) => {
+  if (playerSelection === computerSelection) return "Draw";
+  else if (isPlayerWin(playerSelection, computerSelection)) {
+    return `You win ${playerSelection} beats ${computerSelection}`;
+  } else if (isPlayerLose(playerSelection, computerSelection)) {
+    return `You lose ${playerSelection} beats ${computerSelection}`;
+  }
+};
+
+console.log(playRound(playerSelection, computerSelection));
