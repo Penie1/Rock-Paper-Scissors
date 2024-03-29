80c6c7834b47f43a6711bdf79c836547b7095fac
Peniel Bekele <142409326+Penie1@users.noreply.github.com>
2024-03-30 00:09:17 +0300

    Implement random game choices for computer selection

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const gameChoices = ["rock", "paper", "scissors"];
+const getComputerChoice = (choices) => {
+  const arrLength = choices.length;
+  const randomNum = Math.floor(Math.random() * arrLength);
+  return choices[randomNum];
+};
+getComputerChoice(gameChoices);
